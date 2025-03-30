b7d3ff7a7c054408e08cb51e389c272bb74e71db
Leopold Bauer <Leopold.Bauer@titanom.com>
2025-03-31 00:15:47 +0200

    fix: removed nextauth url

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,12 +5,10 @@ export const env = createEnv({
   server: {
     databaseUrl: z.string(),
     nextauthSecret: z.string().min(1),
-    nextauthUrl: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
     databaseUrl: process.env.DATABASE_URL,
     nextauthSecret: process.env.NEXTAUTH_SECRET,
-    nextauthUrl: process.env.NEXTAUTH_URL,
   },
 });
